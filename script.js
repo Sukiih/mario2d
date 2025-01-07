0c217db799e7aae7df60b77da0db268b4f15c412
Sukiih <vale.sgonzalez2011@hotmail.com>
2025-01-07 00:04:31 -0300

    ADD:evento muerte, musica y brinco tras morir

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 import { createAnimations } from "./animations.js";
 
-//global Phaser config
+// Configuración global de Phaser
 const config = {
     type: Phaser.AUTO,
     width: 256,
     height: 244,
-    backgroundColor: '#049cd8',  //responde a hexadecimales
+    backgroundColor: '#049cd8',
     parent: 'game',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 300},
-            debug: false
+            gravity: { y: 300 },
+            debug: false,
         }
     },
     scene: {
@@ -19,89 +19,109 @@ const config = {
         create, // se ejecua cuando el juego comienza
         update, // se ejecuta para actualizar el juego
     }
-}
+};
 
 new Phaser.Game(config);
 
+// Precarga de recursos
 function preload() {
     this.load.image(
-        'cloud1',
+        'cloud1', 
         'assets/scenery/overworld/cloud1.png'
-    )
-
+    );
     this.load.image(
-        'floorbricks',
+        'floorbricks', 
         'assets/scenery/overworld/floorbricks.png'
-    )
-
+    );
     this.load.spritesheet(
-        'mario',
-        'assets/entities/mario.png',
-        {frameWidth: 18, frameHeight: 16}
-    )    
+        'mario', 'assets/entities/mario.png', 
+        { frameWidth: 18, frameHeight: 16 });
+    this.load.audio(
+        'gameover', 
+        'assets/sound/music/gameover.mp3'
+    );
 }
 
+// Creación de la escena
 function create() {
-    // image(x, y, id del asset)
-   this.add.image(100, 50, 'cloud1')
-   .setOrigin(0, 0)
-   .setScale(0.15)
+    //imagen(x, y, id del asset)
+    this.add.image(100, 50, 'cloud1')
+    .setOrigin(0, 0)
+    .setScale(0.15);
 
-   this.floor = this.physics.add.staticGroup()
+    this.floor = this.physics.add.staticGroup();
 
-   this.floor
-   .create(0, config.height - 16, 'floorbricks')
-   .setOrigin(0, 0.5)
-   //sincronizar fisicas
-   .refreshBody()
+    this.floor.create
+    (0, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody();
 
-   this.floor
-   .create(150, config.height - 16, 'floorbricks')
-   .setOrigin(0, 0.5)
-   .refreshBody()
-   
-  //this.mario = this.add.sprite(50,210,'mario')
+    this.floor.create
+    (150, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody();
 
-   this.mario = this.physics.add.sprite(50, 100,'mario')
-   .setOrigin(0,1)
-   .setGravityY(300)
-   .setCollideWorldBounds(true)
+    // Mario
+    this.mario = this.physics.add.sprite(50, 100, 'mario')
+        .setOrigin(0, 1)
+        .setGravityY(300)
+        .setCollideWorldBounds(true);
 
-   this.physics.world.setBounds(0, 0, 2000, config.height)
-   this.physics.add.collider(this.mario, this.floor)
+    // Configuración del mundo
+    this.physics.world.setBounds(0, 0, 2000, config.height);
+    this.physics.add.collider(this.mario, this.floor);
 
-   this.cameras.main.setBounds(0, 0, 2000, config.height)
-   this.cameras.main.startFollow(this.mario)
-   
-   createAnimations(this)
+    // Cámara
+    this.cameras.main.setBounds(0, 0, 2000, config.height);
+    this.cameras.main.startFollow(this.mario);
 
-   this.keys = this.input.keyboard.createCursorKeys()
+    // Animaciones
+    createAnimations(this);
+
+    // Mov teclado
+    this.keys = this.input.keyboard.createCursorKeys();
+
+    // muerte de Mario
+    this.events.once('mario-dead', () => {
+        this.mario.isDead = true;
+        this.mario.anims.play('mario-dead');
+        this.mario.setCollideWorldBounds(false);
+        this.mario.setVelocity(-350); // brinco al morir
+        this.sound.add('gameover', { volume: 0.3 }).play();
+
+        // Reinicia el juego tras 2 segundos
+        this.time.delayedCall(2000, () => {
+            this.scene.restart();
+        });
+    });
 }
 
+// Actualización del juego
 function update() {
-    if (this.mario.isDead) return
+    if (this.mario.isDead) return;
 
+    // Movimiento izquierda/derecha
     if (this.keys.left.isDown) {
         this.mario.setVelocityX(-100);
         this.mario.anims.play('mario-walk', true);
-        this.mario.flipX = true; // Gira a Mario
+        this.mario.flipX = true; // Voltear a Mario
     } else if (this.keys.right.isDown) {
-        this.mario.setVelocityX(100); 
+        this.mario.setVelocityX(100);
         this.mario.anims.play('mario-walk', true);
-        this.mario.flipX = false; // 
+        this.mario.flipX = false;
     } else {
-        this.mario.setVelocityX(0); // 
-        this.mario.anims.play('mario-idle', true); // Cambia a la animación de inactividad
+        this.mario.setVelocityX(0);
+        this.mario.anims.play('mario-idle', true);
     }
 
+    // Salto
     if (this.keys.up.isDown && this.mario.body.touching.down) {
         this.mario.setVelocityY(-300);
         this.mario.anims.play('mario-jump', true);
     }
 
-    if(this.mario.y >= config.height) {
-        this.mario.isDead = true
-        this.mario.anims.play('mario-dead');
-        this.mario.setCollideWorldBounds(false);      
+    // Detectar muerte
+    if (this.mario.y >= config.height) {
+        this.events.emit('mario-dead'); // Activar el evento de muerte
     }
 }
